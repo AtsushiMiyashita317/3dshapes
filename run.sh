77655255364095548f0577f86a964dc538eec0ce
AtsushiMiyashita317 <miyashita.atsushi@g.sp.m.is.nagoya-u.ac.jp>
2025-12-03 18:31:14 +0900

    add script

---
+++ run.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#PBS -N pl_ddp
+#PBS -l select=4:ncpus=192:ngpus=8:mpiprocs=8
+#PBS -l walltime=12:00:00
+#PBS -j oe
+#PBS -q rt_HF
+#PBS -p gah51684
+
+
+cd $PBS_O_WORKDIR
+
+CKPT_PREDICTOR="./checkpoints/predictor/best-epoch=732-val_loss=0.9436.ckpt"
+BATCH_SIZE=32000
+NUM_WORKERS=32
+STRATEGY="ddp"
+NUM_NODES=4
+DEVICES_PER_NODE=8
+
+# ---- ホストリストの取得 ----
+# PBS_NODEFILE に割り当てノードが列挙されている
+nodes=$(sort -u $PBS_NODEFILE)
+master_addr=$(head -n 1 <<< "$nodes")
+
+# ランク数 (world size)
+nnodes=$(wc -l <<< "$nodes")
+gpus_per_node=8
+world_size=$((nnodes * gpus_per_node))
+
+echo "MASTER: $master_addr"
+echo "NNODES: $nnodes"
+echo "WORLD_SIZE: $world_size"
+
+# ---- PyTorch distributed 実行 ----
+torchrun \
+    --nnodes=$nnodes \
+    --nproc_per_node=$gpus_per_node \
+    --node_rank=$PBS_NODENUM \
+    --master_addr=$master_addr \
+    --master_port=29500 \
+    clnf.py \
+        $CKPT_PREDICTOR \
+        --batch_size $BATCH_SIZE \
+        --num_workers $NUM_WORKERS \
+        --strategy $STRATEGY \
+        --num_nodes $NUM_NODES \
+        --devices $DEVICES_PER_NODE \
