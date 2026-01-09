4794c9546190dfea20b79601c39971471e588323
miyashita-elie <miyashita-elie@aist.go.jp>
2026-01-09 10:34:01 +0900

    add scripts

--- run_single.sh
+++ run_single.sh
@@ -11,18 +11,35 @@
 cd $PBS_O_WORKDIR
 
 ckpt_predictor="./checkpoints/predictor/best-epoch=732-val_loss=0.9436.ckpt"
+ckpt_autoencoder="./checkpoints/autoencoder/best-epoch=599-val_loss=0.0020.ckpt"
 batch_size=16000
 num_workers=4
 lr=0.004
-latent_dim=12
-num_bases=66
+
+num_bases_sym=276
+num_bases_null=276
+eps_p_sym=0.001
+eps_q_sym=10.0
+eps_p_null=0.001
+eps_q_null=0.1
+
+run_name="eps_q-${eps_q_sym}"
 
 source venv/bin/activate
 
 python clnf.py \
     ${ckpt_predictor} \
+    ${ckpt_autoencoder} \
+    --run_name ${run_name} \
     --batch_size ${batch_size} \
     --num_workers ${num_workers} \
     --lr ${lr} \
-    --latent_dim ${latent_dim} \
-    --num_bases ${num_bases}
+    --num_bases_sym ${num_bases_sym} \
+    --eps_p_sym ${eps_p_sym} \
+    --eps_q_sym ${eps_q_sym} \
+    # --rescale_eps \
+    # --normalize_precision \
+    # --normalize_generators \
+    # --num_bases_null ${num_bases_null} \
+    # --eps_p_null ${eps_p_null} \
+    # --eps_q_null ${eps_q_null} \
