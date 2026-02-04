76d62b6f129a781697ceef6d19c6f59f4397cbf9
miyashita-elie <miyashita-elie@aist.go.jp>
2026-02-04 09:37:41 +0900

    verify dataset effects

---
+++ scripts/dataset/clnf/color.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#PBS -N pl_single
+#PBS -l select=1
+#PBS -l walltime=24:00:00
+#PBS -j oe
+#PBS -k oed
+#PBS -q rt_HG
+#PBS -P gah51684
+
+
+cd $PBS_O_WORKDIR
+
+removed_factors="floor_hue wall_hue object_hue"
+run_name="color"
+
+ckpt_predictor="./checkpoints/predictor/best-epoch=732-val_loss=0.9436.ckpt"
+ckpt_autoencoder="./checkpoints/autoencoder/${run_name}.ckpt"
+batch_size=16000
+num_workers=4
+lr=0.004
+
+num_bases_sym=276
+num_bases_null=276
+eps_p_sym=0.001
+eps_q_sym=0.1
+eps_p_null=0.001
+eps_q_null=0.1
+
+source venv/bin/activate
+
+python clnf.py \
+    ${ckpt_predictor} \
+    ${ckpt_autoencoder} \
+    --run_name ${run_name} \
+    --batch_size ${batch_size} \
+    --num_workers ${num_workers} \
+    --lr ${lr} \
+    --num_bases_sym ${num_bases_sym} \
+    --eps_p_sym ${eps_p_sym} \
+    --eps_q_sym ${eps_q_sym} \
+    --removed_factors ${removed_factors} \
